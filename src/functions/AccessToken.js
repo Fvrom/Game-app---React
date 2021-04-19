b2d219d22f31c9b33b84609db3eb9cf84184bd1a
MadosMark <69796358+MadosMark@users.noreply.github.com>
2021-04-19 17:38:51 +0200

    Code Clean Up

--- src/functions/AccessToken.js
+++
@@ -1,9 +0,0 @@
-import queryString from "query-string";
-
-const getAccessToken = () => {
-  const { access_token } = queryString.parse(window.location.hash);
-
-  return access_token;
-};
-
-export default getAccessToken;
